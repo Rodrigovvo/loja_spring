3305fd8e1d5979ff93aceb85889d99ae8c9795c7
Rodrigovvo <rod_vinicius@hotmail.com>
2021-01-04 00:15:00 -0300

    Controller save Usuario

--- src/main/java/com/dev/loja/controller/PrincipalController.java
+++ src/main/java/com/dev/loja/controller/PrincipalController.java
@@ -1,10 +1,22 @@
 package com.dev.loja.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dev.loja.model.Funcionario;
+import com.dev.loja.repository.FuncionarioRepository;
 
 @Controller
 public class PrincipalController {
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepo;
 
     @GetMapping("/administrativo")
     public String acessarPrincipal(){
@@ -12,13 +24,25 @@ public class PrincipalController {
     }
     
 
-    @GetMapping("/usuarios/listar")
-    public String acessarCadastroUsucario(){
-        return "administrativo/usuarios/lista";
+    @GetMapping("/usuarios/cadastrar")
+    public ModelAndView acessarCadastroUsucario (Funcionario funcionario){
+        ModelAndView mv = new ModelAndView("administrativo/usuarios/cadastro");
+        mv.addObject("funcionario", funcionario);
+        
+    	return mv;
+    }
+    @PostMapping("/usuarios/cadastrar")
+    public ModelAndView salvarUsuario(@Valid Funcionario funcionario, BindingResult resul) {
+    	if(resul.hasErrors()) {
+    		return acessarCadastroUsucario(funcionario);
+    	}else {
+    		funcionarioRepo.save(funcionario);
+    		return acessarCadastroUsucario(new Funcionario());
+    	}
     }
     
-    @GetMapping("/usuarios/cadastrar")
+    @GetMapping("/usuarios/listar")
     public String acessarListaUsucarios(){
-        return "administrativo/usuarios/cadastro";
+        return "administrativo/usuarios/lista";
     }
 }
