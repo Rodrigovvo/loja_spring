402a03a82f0fde30525d8ba7d9a764239f0c96bb
Rodrigovvo <rod_vinicius@hotmail.com>
2021-01-14 21:16:01 -0300

    View Carrinho de Compras

--- src/main/java/com/dev/loja/controller/ProdutoController.java
+++ src/main/java/com/dev/loja/controller/ProdutoController.java
@@ -1,7 +1,5 @@
 package com.dev.loja.controller;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
