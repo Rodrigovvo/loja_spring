236752a41a6924703f5e559474ce00d9f745224b
Rodrigovvo <rod_vinicius@hotmail.com>
2021-01-16 14:45:46 -0300

    Atualização Layout

--- src/main/java/com/dev/loja/controller/CarrinhoController.java
+++ src/main/java/com/dev/loja/controller/CarrinhoController.java
@@ -2,6 +2,7 @@ package com.dev.loja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +11,11 @@ public class CarrinhoController {
 	@GetMapping("/carrinho")
 	public ModelAndView acessoCarrinhoCompras() {
 		return new ModelAndView("clientes/carrinho");
-
+	}
+	
+	@GetMapping("/carrinho/adicionar/{id}")
+	public ModelAndView adicionarItemCarrinhoCompras(@PathVariable long id) {
+		System.out.println(id);
+		return new ModelAndView("clientes/carrinho");
 	}
 }
