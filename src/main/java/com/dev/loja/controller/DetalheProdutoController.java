e4bb3bfc0204b8d2b5ebd8a2b748e962f81f557e
Rodrigovvo <rod_vinicius@hotmail.com>
2021-01-18 22:04:39 -0300

    Correções Layout

--- src/main/java/com/dev/loja/controller/DetalheProdutoController.java
+++ src/main/java/com/dev/loja/controller/DetalheProdutoController.java
@@ -2,13 +2,14 @@ package com.dev.loja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DetalheProdutoController {
 
-	@GetMapping("/detalhe")
-	public ModelAndView acessoNegado() {
+	@GetMapping("/detalhe/{id}")
+	public ModelAndView acessoNegado(@PathVariable Long id) {
 		return new ModelAndView("/clientes/detalheProduto");
 	}
 
